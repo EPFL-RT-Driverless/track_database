4c31a39e2e16e201c1c55abd993a55f8b7617d69
Ted <oancea.tudor@icloud.com>
2022-08-08 10:52:40 +0200

    cmmit

--- tests/plot_tracks.py
+++ tests/plot_tracks.py
@@ -3,7 +3,7 @@ from track_database import *
 
 if __name__ == "__main__":
     # load freshly generated skidpad ======================================================
-    center_line, left_cones, right_cones = skidpad()
+    # center_line, left_cones, right_cones = skidpad()
 
     # load skidpad from file ===================================================
     # center_line, left_cones, right_cones = load_track(
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     # center_line, left_cones, right_cones = import_default_skidpad()
 
     # load default FS track from file ===================================================
-    # center_line, left_cones, right_cones = import_default_fs_track()
+    center_line, left_cones, right_cones = import_default_fs_track()
 
     plt.scatter(center_line[0, :], center_line[1, :], color="red", marker="+")
     plt.scatter(left_cones[0, :], left_cones[1, :], color="blue", marker="^")
