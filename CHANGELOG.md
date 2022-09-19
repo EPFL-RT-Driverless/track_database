96b462acab58cac92df6c6491b9c2a2ab5b4b2c7
Ted <oancea.tudor@icloud.com>
2022-09-19 14:22:15 +0200

    :memo: updated everything to match `python_boilerplate` v2.0.1 and removed `ReferencePath` tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# v2.0.0
+updated everything to match `python_boilerplate` v2.0.1 and removed ReferencePath tests
+
 # v1.1.0
 - changed data storage to include left and right widths
 - the `center_line`, `left_cones` and `right_cones` are now returned in column major
--- setup.py
+++ setup.py
@@ -11,13 +11,12 @@ with open("requirements.txt") as f:
 
 setup(
     name="track_database",
-    version="1.1.0",
+    version="2.0.0",
     packages=["track_database"],
     package_dir={"track_database": "track_database"},
     package_data={"track_database": ["data/*.csv"]},
     url="https://github.com/EPFL-RT-Driverless/track_database",
     author="tudoroancea",
-    author_email="oancea.tudor@icloud.com",
     description="A Python package to gather some default Formula Student tracks as well as some utility functions to import/export them in CSV format and to create new custom tracks.",
     long_description=readme,
     install_requires=requirements,
