0fd218b47ddcf2b15c43fb87452b891e4e387103
Ted <oancea.tudor@icloud.com>
2022-11-23 16:34:05 +0100

    bruh

--- tests/plot_tracks.py
+++ tests/plot_tracks.py
@@ -4,15 +4,15 @@ from track_database import *
 
 if __name__ == "__main__":
     # load default skidpad from file ===================================================
-    # center_line, widths, right_cones, left_cones = load_default_skidpad()
+    # center_line.csv, widths, right_cones, left_cones = load_default_skidpad()
     (
         center_line,
         widths,
         right_cones,
         left_cones,
     ) = load_default_short_skidpad()
-    # center_line, widths, right_cones, left_cones = load_default_acceleration_track()
-    # (center_line, widths, right_cones, left_cones) = load_default_fs_track()
+    # center_line.csv, widths, right_cones, left_cones = load_default_acceleration_track()
+    # (center_line.csv, widths, right_cones, left_cones) = load_default_fs_track()
 
     plt.plot(center_line[:, 0], center_line[:, 1], color="red", marker="+")
     plt.scatter(left_cones[:, 0], left_cones[:, 1], color="blue", marker="^")
