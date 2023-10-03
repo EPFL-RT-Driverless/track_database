a8d0d396f8de1b8eb120ae285a7f8f290ae33b07
Charles <charles.boissier@epfl.ch>
2023-10-03 23:06:15 +0200

    added new (small) autoX track for Vaudoise event

--- track_database/tracks.py
+++ track_database/tracks.py
@@ -19,6 +19,7 @@ available_tracks = [
     "VSV",
     "VSV_XS",
     "VSV_XL",
+    "autoX_Vaudoise_Sponso",
 ]
 
 
---
+++ work/autoX_03_10_23/viz_track.py
@@ -0,0 +1,14 @@
+from track_database.utils import load_cones, plot_cones, load_center_line
+from track_database.tracks import load_track
+import numpy as np
+import matplotlib.pyplot as plt
+
+track = load_track("autoX_Vaudoise_Sponso")
+# blue_cones, yellow_cones, big_orange_cones, _, _, _ = load_cones("21_05_2023.csv")
+plot_cones(
+    track.blue_cones, track.yellow_cones, track.big_orange_cones, np.empty((0, 2)), show=False
+)
+# center_line, _ = load_center_line("21_05_2023_center_line.csv")
+plt.plot(track.center_line[:, 0], track.center_line[:, 1], "k--")
+plt.scatter(track.center_line[:, 0], track.center_line[:, 1], c="k")
+plt.show()
